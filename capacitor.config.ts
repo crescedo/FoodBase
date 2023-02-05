f414da69cf0c25df602b5acab27e7a70e5eff752
Edoardo Crescenti <crescedo@students.zhaw.ch>
2023-02-05 10:22:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.foodbase',
+  appName: 'foodbase',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
