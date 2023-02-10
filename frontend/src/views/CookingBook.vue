1b55f3657f5c925fc3ad7f62ba54cec4d6ed6a6b
Jessica Schalcher <schaljes@students.zhaw.ch>
2023-02-10 09:45:55 +0100

    Änderung Shema auf Cards

--- frontend/src/views/CookingBook.vue
+++ frontend/src/views/CookingBook.vue
@@ -2,27 +2,23 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title class="boldText"
-          ><ion-icon :icon="book" color="warning"></ion-icon> Kochbuch
-          <ion-icon :icon="book" color="warning"></ion-icon
-        ></ion-title>
+        <ion-title class="boldText">
+          <ion-icon :icon="book" color="warning"></ion-icon> Kochbuch
+          <ion-icon :icon="book" color="warning"></ion-icon>
+        </ion-title>
         <!--       <div>
-              <ion-button size="small" color="green" v-bind:router-link="viewRecipe">
-                <ion-icon  slot="icon-only" :icon="addCircleOutline" />
-              </ion-button>
-              </div>-->
+                <ion-button size="small" color="green" v-bind:router-link="viewRecipe">
+                  <ion-icon  slot="icon-only" :icon="addCircleOutline" />
+                </ion-button>
+                </div>-->
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" class="bgcolor">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
       <ion-grid>
         <ion-row>
           <ion-col size="9">
-            <ion-searchbar class="scolor"
+            <ion-searchbar
+              class="scolor"
               :search-icon="searchCircle"
               placeholder="Suchen"
             ></ion-searchbar>
@@ -40,44 +36,47 @@
           </ion-col>
         </ion-row>
       </ion-grid>
-      <!-- ion-grid v-for""" -->
-      <ion-grid v-for="recipe in recipes" :key="recipe.id">
-        <ion-row>
-          <ion-col>
-            <ion-img> {{ recipe.thumbnailUrl?.url }} </ion-img>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="9" class="boldText" v-bind:router-link="viewRecipe">             {{ recipe.title }} </ion-col>
-          <ion-col size="3" class>
-            <div>
-              <ion-button size="small" color="danger" v-bind:router-link="filterPath">
-                <ion-icon  slot="icon-only" :icon="heart" />
-              </ion-button>
-              </div>
-          </ion-col>
+      <ion-card v-for="recipe in recipes" :key="recipe.id">
+        <ion-img> {{ recipe.thumbnailUrl?.url }} </ion-img>
+        <ion-card-header>
+          <ion-card-title>
+            {{ recipe.title
+            }}   <ion-button
+              size="small"
+              color="danger"
+              v-bind:router-link="filterPath">
+              <ion-icon slot="icon-only" :icon="heart" />
+            </ion-button>
+          </ion-card-title>
+          <ion-card-subtitle>{{
+              recipe.category
+            }}</ion-card-subtitle>
+                    <ion-card-subtitle v-bind:router-link="viewRecipe"
+            ><ion-icon :icon="time" />{{
+              recipe.cookingTime
+            }}</ion-card-subtitle
+          >
+          <ion-card-subtitle v-bind:router-link="viewRecipe"
+            ><ion-icon :icon="barbell" />
+            {{ recipe.difficulty }}</ion-card-subtitle>
+        </ion-card-header>
 
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <ion-icon :icon="time" />
-            {{ recipe.cookingTime }}
-            <!-- cookingtime""" -->
-            30 Min
-          </ion-col>
-          <ion-col>
-            <ion-icon :icon="barbell" />
-            {{ recipe.difficulty }}
-             <!-- einfach -->
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+        <ion-card-content> </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
-  
-  <script setup lang="ts">
-import { book, time, barbell, filter, addCircleOutline, heart } from "ionicons/icons";
+
+ 
+<script setup lang="ts">
+import {
+  book,
+  time,
+  barbell,
+  filter,
+  addCircleOutline,
+  heart,
+} from "ionicons/icons";
 import {
   IonIcon,
   IonSearchbar,
@@ -98,25 +97,32 @@ import {
   IonTitle,
   IonToolbar,
   IonTabButton,
+  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, 
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { searchCircle } from "ionicons/icons";
-import { useRecipes} from "../composables/useRecipes";
-const {recipes,getRecipes}=useRecipes();
-const filterPath ="/tabs/filterrecipe"
+import { useRecipes } from "../composables/useRecipes";
+const { recipes, getRecipes } = useRecipes();
+const filterPath = "/tabs/filterrecipe";
 
 const viewRecipe = "/tabs/recipeDetail";
 </script>
 
   
   <style scoped>
-  .bgcolor{
-    --ion-background-color: #FFFED9
+
+ion-card-header.ios {
+    display: flex;
+    flex-flow: column-reverse;
+  }
+
+.bgcolor {
+  --ion-background-color: #fffed9;
 }
-  .boldText{
-    font-weight: 700;
-  }
-    .scolor{
-    --ion-background-color: #FFFFFF
-  }
+.boldText {
+  font-weight: 700;
+}
+.scolor {
+  --ion-background-color: #ffffff;
+}
 </style>
